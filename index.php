0052f34bd5c2de8b2b414509aec347b49f47cfba
smithwebtek <brad@smithwebtek.com>
2020-10-10 16:17:38 -0400

    Add to php

--- index.php
+++ index.php
@@ -12,7 +12,24 @@
 
 
 	<? echo "Hello, World!"; ?>
+	<? echo "Hello, World!"; ?>
+	<? echo "Hello, World!"; ?>
+	<? echo "Hello, World!"; ?>
+	<?php echo $_SERVER['HTTP_USER_AGENT']; ?>
 
+	<?php
+if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE) {
+?>
+<h3>strpos() must have returned non-false</h3>
+<p>You are using Internet Explorer</p>
+<?php
+} else {
+?>
+<h3>strpos() must have returned false</h3>
+<p>You are not using Internet Explorer</p>
+<?php
+}
+?>
 
 </body>
  
